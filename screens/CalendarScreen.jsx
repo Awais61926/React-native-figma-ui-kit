d309dbef20276feb4772c64408b4a11743a2ba91
Muhammad Awais <awais619261@gmail.com>
2024-09-19 11:06:09 -0700

    updated

---
+++ screens/CalendarScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+
+const CalendarScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#5DB075' }}>
+      <Calendar 
+        style={{width:'100%', height:'90%'}}
+        current={'2024-09-18'}
+      />
+    </View>
+  );
+};
+
+
+export default CalendarScreen;
--- screens/Market.jsx
+++ screens/Market.jsx
@@ -4,74 +4,133 @@ import { useEffect, useState } from "react";
 import CustomHeader from "../components/CustomHeader";
 import CustomSearchBar from "../components/CustomSearchBar";
 import { StyleSheet } from "react-native";
-import { FlatList,Image } from "react-native";
+import { FlatList, Image } from "react-native";
 
-const Market=({navigation})=>{
-    const [product, setProduct]= useState([])
-    const [search, setSearch]=useState('')
-    useEffect(()=>{
+const Market = ({ navigation }) => {
+    const [product, setProduct] = useState([]);
+    const [trendingProduct, setTrendingProduct] = useState([]);
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
         getData();
-    },
-    []
-    );
-    const getData=()=>{
-        fetch('https://fakestoreapi.com/products')
-        .then(res=>res.json())
-        .then(json=>
-            {setProduct(json);
-            });
+    }, []);
+
+    const getData = () => {
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((json) => {
+
+                setProduct(json);
+
+
+                const filteredProducts = json.filter(
+                    (product) => product.rating.rate > 4
+                );
+                setTrendingProduct(filteredProducts); 
+            })
+            .catch((error) => console.log(error));
     };
-    return(
+
+    return (
         <View>
             <View>
                 <CustomHeader
-                righttext="Filter"
-                centertitle="Market"
-                lefttext="Back"
-                onRightTextPress={()=>{navigation.goBack()}}/>
+                    righttext="Filter"
+                    centertitle="Market"
+                    lefttext="Back"
+                    onRightTextPress={() => {
+                        navigation.goBack();
+                    }}
+                />
             </View>
-            <View style={{justifyContent:'center', alignItems:'center'}}>
-                <CustomSearchBar/>
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <CustomSearchBar />
             </View>
             <View>
                 <Text style={styles.heading}>Hot Deals</Text>
             </View>
-            <View style={{width:'100%', height:177}}>
+            <View style={{ width: "100%", height: 177 }}>
+                {/* Hot Deals Section */}
                 <FlatList
-                horizontal
-                data={product} renderItem={({item,index})=>
-                    <View style={{width:110, height:177,padding:2,margin:5}}>
-                        <View>
-                        <Image source={{uri:item.image}} style={{width:100, height:100}}/>
-                        </View>
-                        
-                        <View>
-                        <Text style={{color:'black',fontSize:14,fontWeight:'regular'}}>{item.title.length>20?item.title.substring(0, 20):item.title}...</Text>
-                        <Text style={{fontSize:14,fontWeight:'bold',color:'black'}}>${item.price}</Text>
-                        </View>
+                    horizontal
+                    data={product}
+                    renderItem={({ item, index }) => (
+                        <View style={{ width: 110, height: 177, padding: 2, margin: 5 }}>
+                            <View>
+                                <Image
+                                    source={{ uri: item.image }}
+                                    style={{ width: 100, height: 100 }}
+                                />
+                            </View>
 
-                    </View>
-                    
-                }
-                
+                            <View>
+                                <Text style={{ color: "black", fontSize: 14 }}>
+                                    {item.title.length > 20
+                                        ? item.title.substring(0, 20)
+                                        : item.title}
+                                    ...
+                                </Text>
+                                <Text
+                                    style={{
+                                        fontSize: 14,
+                                        fontWeight: "bold",
+                                        color: "black",
+                                    }}
+                                >
+                                    ${item.price}
+                                </Text>
+                            </View>
+                        </View>
+                    )}
                 />
             </View>
 
-
+            {/* Trending Section */}
+            <Text style={styles.heading}>Trending</Text>
+            <View>
+                <FlatList
+                    horizontal={true}
+                    data={trendingProduct}
+                    renderItem={({ item, index }) => (
+                        <View style={{ padding: 2, margin: 2 }}>
+                            <Image
+                                source={{ uri: item.image }}
+                                style={{ width: 100, height: 100 }}
+                            />
+                            <View>
+                                <Text style={{ color: "black", fontSize: 14 }}>
+                                    {item.title.length > 20
+                                        ? item.title.substring(0, 20)
+                                        : item.title}
+                                    ...
+                                </Text>
+                                <Text
+                                    style={{
+                                        fontSize: 14,
+                                        fontWeight: "bold",
+                                        color: "black",
+                                    }}
+                                >
+                                    ${item.price}
+                                </Text>
+                            </View>
+                        </View>
+                    )}
+                />
+            </View>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
-    heading:{
-        fontSize:25,
-        fontWeight:'medium',
-        color:'black',
-        paddingLeft:15,
-        paddingTop:2,
-        fontStyle:'normal',
-
-
+    heading: {
+        fontSize: 25,
+        fontWeight: "medium",
+        color: "black",
+        paddingLeft: 15,
+        paddingTop: 2,
+        fontStyle: "normal",
     },
-})
+});
+
 export default Market;
